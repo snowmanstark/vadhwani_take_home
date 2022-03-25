10d0a7cb72daede952beae9adbae929e248e747a
Philemon Johnson <phil.j@cloudera.com>
2022-03-25 23:17:07 +0530

    refined HTML page for View and upload

--- fastapi-server/agro/view/viewData.py
+++ fastapi-server/agro/view/viewData.py
@@ -1,5 +1,6 @@
 from fastapi import routing, Request
 from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 
 from agro.dependencies import db, src_lang, target_langs
 
@@ -8,16 +9,19 @@ router = routing.APIRouter(
     tags=["View farmer Data"]
 )
 
+template = Jinja2Templates("agro/templates")
 users = db["users"]
 
 @router.get("/")
-def view_farmer_data(lang: str, request: Request):
+def view_farmer_data(request: Request, lang: str = "en"):
     if lang in target_langs or lang == src_lang:
         query = list(users.find({},{"_id":0,"phone":1, lang: 1}))
         for user in query:
             info = user.pop(lang)
             user.update(info)
-        return query
+        
+        context={"request": request, "users": query, "lang": lang}
+        # return query
+        return template.TemplateResponse("view.html", context=context)
     else:
         return Response(status_code=404,content="Invalid language")
-
